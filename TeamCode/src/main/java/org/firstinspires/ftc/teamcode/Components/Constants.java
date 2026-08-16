aa7e4089d483dcd06099f91644f819cb1cae1a61
akulm-eng <akul.m@mckinneysteamacademy.org>
2026-08-16 18:36:39 -0500

    fixing auton problems

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Components/Constants.java
@@ -8,7 +8,8 @@ import com.pedropathing.ftc.drivetrains.MecanumConstants;
 import com.pedropathing.paths.PathConstraints;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
+import com.pedropathing.ftc.localization.Encoder;
+import com.pedropathing.ftc.localization.constants.DriveEncoderConstants;
 public class Constants {
 
     public static FollowerConstants followerConstants = new FollowerConstants()
@@ -26,10 +27,22 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.REVERSE);
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
 
+    public static DriveEncoderConstants localizerConstants =
+            new DriveEncoderConstants()
+                    .rightFrontMotorName("rightFront")
+                    .rightRearMotorName("rightBack")
+                    .leftRearMotorName("leftBack")
+                    .leftFrontMotorName("leftFront")
+                    .leftFrontEncoderDirection(Encoder.FORWARD)
+                    .leftRearEncoderDirection(Encoder.FORWARD)
+                    .rightFrontEncoderDirection(Encoder.FORWARD)
+                    .rightRearEncoderDirection(Encoder.FORWARD);
+
     public static Follower createFollower(HardwareMap hardwareMap) {
         return new FollowerBuilder(followerConstants, hardwareMap)
                 .pathConstraints(pathConstraints)
                 .mecanumDrivetrain(driveConstants)
+                .driveEncoderLocalizer(localizerConstants)
                 .build();
     }
 }
