331d963924a7b101295cf44b2a97a24db14c5684
MohammadSyed18 <79668964+MohammadSyed18@users.noreply.github.com>
2026-08-30 18:17:48 -0500

    updates to drivetrain

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestTeleOp1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/TestTeleOp1.java
@@ -1,50 +1,26 @@
 ```java
-package org.firstinspires.ftc.teamcode.Tests;
+        package org.firstinspires.ftc.teamcode.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.Components.DriveTrain;
-import org.firstinspires.ftc.teamcode.Components.Intake;
-import org.firstinspires.ftc.teamcode.Components.Outtake;
-import org.firstinspires.ftc.teamcode.Components.Pusher;
-import org.firstinspires.ftc.teamcode.Components.ToucherXD;
 
 @TeleOp(name = "TestTeleOp1")
 public class TestTeleOp1 extends LinearOpMode {
 
     private DriveTrain drivetrain;
-    private Intake intake;
-    private Pusher pusher;
-    private ToucherXD toucherXD;
-    private Outtake outtake;
-    private DcMotorEx turretMotor;
-
-    // Tracks previous right bumper state
-    private boolean wasRBPressedLastLoop = false;
 
     @Override
     public void runOpMode() {
 
         // ----------------------------
-        // INITIALIZE COMPONENTS
+        // INITIALIZE DRIVETRAIN
         // ----------------------------
         drivetrain = new DriveTrain(hardwareMap);
-        intake = new Intake(hardwareMap);
-        pusher = new Pusher(hardwareMap);
-        toucherXD = new ToucherXD(hardwareMap);
-        outtake = new Outtake(hardwareMap);
-
         drivetrain.initialize();
-        intake.initialize();
-        pusher.initialize();
-        toucherXD.initialize();
 
-        turretMotor = hardwareMap.get(DcMotorEx.class, "turretMotor");
-        turretMotor.setPower(0);
-
-        telemetry.addLine("READY...");
+        telemetry.addLine("DRIVETRAIN READY");
         telemetry.update();
 
         waitForStart();
@@ -55,28 +31,13 @@ public class TestTeleOp1 extends LinearOpMode {
             // MECANUM DRIVING
             // ----------------------------
 
-            /*
-             * FORWARD / BACKWARD
-             *
-             * IMPORTANT:
-             * We are NOT reversing this anymore.
-             *
-             * If pushing the stick forward now makes the robot
-             * move forward correctly, leave this exactly as-is.
-             */
+            // Forward / backward
             double movement = gamepad1.left_stick_y;
 
-            /*
-             * STRAFING
-             *
-             * This is reversed independently because your strafe
-             * direction was previously backwards.
-             */
+            // Left / right strafing
             double strafe = -gamepad1.left_stick_x;
 
-            /*
-             * ROTATION
-             */
+            // Rotation
             double rotation = gamepad1.right_stick_x;
 
             drivetrain.TeleOpControl(
@@ -87,69 +48,27 @@ public class TestTeleOp1 extends LinearOpMode {
             );
 
             // ----------------------------
-            // DISKMOTOR CONTROL
-            // Gamepad 1 Right Bumper
-            // ----------------------------
-            boolean rbPressed = gamepad1.right_bumper;
-
-            if (rbPressed && !wasRBPressedLastLoop) {
-                toucherXD.movenextstep();
-            }
-
-            wasRBPressedLastLoop = rbPressed;
-
-            // ----------------------------
-            // INTAKE
-            // ----------------------------
-            if (gamepad1.dpad_up) {
-                intake.intakeIn();
-            }
-            else if (gamepad1.dpad_down) {
-                intake.intakeOut();
-            }
-            else {
-                intake.stop();
-            }
-
-            // ----------------------------
-            // TURRET ROTATION
-            // ----------------------------
-            if (gamepad2.left_trigger > 0.1) {
-                turretMotor.setPower(-0.5);
-            }
-            else if (gamepad2.right_trigger > 0.1) {
-                turretMotor.setPower(0.5);
-            }
-            else {
-                turretMotor.setPower(0);
-            }
-
-            // ----------------------------
-            // PUSHER
-            // ----------------------------
-            pusher.update(gamepad2);
-
-            // ----------------------------
-            // TOUCHER XD
-            // ----------------------------
-            toucherXD.update();
-
-            // ----------------------------
-            // OUTTAKE
-            // ----------------------------
-            outtake.update(gamepad2);
-
-            // ----------------------------
             // TELEMETRY
             // ----------------------------
 
             telemetry.addLine("=== DRIVE INPUTS ===");
 
-            telemetry.addData("Drive", movement);
-            telemetry.addData("Strafe", strafe);
-            telemetry.addData("Rotation", rotation);
+            telemetry.addData(
+                    "Movement",
+                    movement
+            );
 
-            telemetry.addLine("=== DRIVE MOTOR POWERS ===");
+            telemetry.addData(
+                    "Strafe",
+                    strafe
+            );
+
+            telemetry.addData(
+                    "Rotation",
+                    rotation
+            );
+
+            telemetry.addLine("=== MOTOR POWERS ===");
 
             telemetry.addData(
                     "Left Front",
@@ -171,72 +90,9 @@ public class TestTeleOp1 extends LinearOpMode {
                     drivetrain.getRightBackPower()
             );
 
-            telemetry.addLine("=== OTHER MOTORS ===");
-
-            telemetry.addData(
-                    "Turret Power",
-                    turretMotor.getPower()
-            );
-
-            telemetry.addData(
-                    "Shooter Motor Power",
-                    outtake.getMotorPower()
-            );
-
-            telemetry.addData(
-                    "Shooter Servo Angle",
-                    outtake.getShooterServoPosition()
-            );
-
-            telemetry.addData(
-                    "Lazy Susan Power",
-                    outtake.getLazySusanPower()
-            );
-
-            telemetry.addData(
-                    "Pusher State",
-                    pusher.getStateString()
-            );
-
-            telemetry.addData(
-                    "Pusher Position",
-                    pusher.getCurrentPosition()
-            );
-
-            telemetry.addLine("=== TOUCHER DIAGNOSTIC ===");
-
-            telemetry.addData(
-                    "Touch Sensor Pressed",
-                    toucherXD.isTouchPressed()
-            );
-
-            telemetry.addData(
-                    "Target Position",
-                    toucherXD.getTargetPosition()
-            );
-
-            telemetry.addData(
-                    "Current Position",
-                    toucherXD.getCurrentEncoderPosition()
-            );
-
-            telemetry.addData(
-                    "Motor Power",
-                    toucherXD.getMotorPower()
-            );
-
-            telemetry.addData(
-                    "Spacing Ticks",
-                    toucherXD.getSpacingTicks()
-            );
-
             telemetry.update();
         }
 
-        // Safety stop after TeleOp ends
         drivetrain.stop();
-        turretMotor.setPower(0);
-        intake.stop();
     }
 }
-```
